95c1fa327353e66e9ddd7386ab2c1b0e14537b77
mandardeulkar <deulkarm@gmail.com>
2025-03-04 00:37:59 +0530

    Display table data using map() function

--- components/app.js
+++ components/app.js
@@ -1,11 +1,20 @@
 import Banner from "./banner";
 import Footer from "./footer";
+import HouseList from "./HouseList";
 
 const App = () => {
     return (
 
         <div>
+
+            {/* props example -  */}
             <Banner headerText="Providing houses all over the world" />
+            
+            {/* Table Component to display data */}
+            <HouseList />
+
+            {/* children allows JSX/HTML code syntax written in between the tags.
+            like below code -  */}
             <Footer>
                 <div>
                     <i>Globalmatics. All rights reserved.</i>
