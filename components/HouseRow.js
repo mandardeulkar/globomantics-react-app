a634331e3d6fd1df632237f217d4ad3c5b68b531
mandardeulkar <deulkarm@gmail.com>
2025-03-06 14:56:57 +0530

    Added useMemo Hook for home.price calculation

--- components/HouseRow.js
+++ components/HouseRow.js
@@ -1,15 +1,22 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { ReactContext } from "./app";
 
 const HouseRow = ({ house }) => {
 
     const { homeState, setHomeState } = useContext(ReactContext);
 
+    // Memoizing the expensive price calculation
+    const memoizeHousePrice = useMemo(()=>{
+        console.log("Recalculating House Price...");
+        return house.price*1.5;
+    },[house.price]); // Recomputes only when `house.price` changes
+
     return (
         <tr>
             <td>{house.address}</td>
             <td>{house.country}</td>
-            <td>{house.price}</td>
+            {/* <td>{house.price*1.5}</td> { /* Suppose this is CPU-intense calculation */}
+            <td>{memoizeHousePrice}</td> {/* Uses Memoised Value */}
             <td>{homeState.homeCondition}</td>
             <td>{homeState.bidStatus}</td>
             <td>
